2df8f9f05cf43067bee7ac548a6ff4f04bffe306
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/timer_it.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>timer </source>
+        <comment>The trigger. Lowercase.</comment>
+        <translation>timer </translation>
+    </message>
+    <message>
+        <source>&lt;duration&gt; [name]</source>
+        <translation>&lt;durata&gt; [nome]</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <comment>Action verb form</comment>
+        <translation>Avvia</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <comment>Action verb form</comment>
+        <translation>Rimuovi</translation>
+    </message>
+    <message>
+        <source>Set timer</source>
+        <translation>Imposta timer</translation>
+    </message>
+    <message>
+        <source>Timer</source>
+        <translation>Timer</translation>
+    </message>
+    <message>
+        <source>Expires at %1</source>
+        <translation>Scade alle %1</translation>
+    </message>
+    <message>
+        <source>Expired at %1</source>
+        <translation>Scaduto alle %1</translation>
+    </message>
+</context>
+</TS>
